181d4ff44c52ceee704380aa83565b4fd139fb4b
kirti-sahu-178 <kirtisahu178@gmail.com>
2025-01-07 15:43:21 +0530

    add a new component - timepicker

---
+++ src/app/components/timepicker/timepicker.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-timepicker',
+  standalone: true,
+  imports: [],
+  templateUrl: './timepicker.component.html',
+  styleUrl: './timepicker.component.scss'
+})
+export class TimepickerComponent {
+
+}
