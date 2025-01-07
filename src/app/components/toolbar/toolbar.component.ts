3110147d4606574a8c102f01e5371e891a93cdb8
kirti-sahu-178 <kirtisahu178@gmail.com>
2025-01-07 15:34:55 +0530

    update toolbar page

--- src/app/components/toolbar/toolbar.component.ts
+++ src/app/components/toolbar/toolbar.component.ts
@@ -2,54 +2,86 @@ import { Component } from '@angular/core';
 import { MaterialModule } from '../../material/material.module';
 import { StickyCardComponent } from '../sticky-card/sticky-card.component';
 import { CodeTabDesignComponent } from '../code-tab-design/code-tab-design.component';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-toolbar',
   standalone: true,
-  imports: [MaterialModule, StickyCardComponent, CodeTabDesignComponent],
+  imports: [CommonModule, MaterialModule, StickyCardComponent, CodeTabDesignComponent],
   templateUrl: './toolbar.component.html',
   styleUrl: './toolbar.component.scss'
 })
 export class ToolbarComponent {
   isCopied: boolean = false;
   HtmlCodeToCopy1 = `
-    <mat-toolbar>
-      <button mat-icon-button>
-        <mat-icon matTooltip="A little door to endless possibilities!">menu</mat-icon>
-      </button>
-      <span>My App</span>
-      <span class="example-spacer"></span>
-      <button mat-icon-button>
-        <mat-icon matTooltip="If you love yourself, the universe reflects that love back to you. ">favorite</mat-icon>
-      </button>
-      <button mat-icon-button>
-        <mat-icon matTooltip="Share your knowledge with others">share</mat-icon>
-      </button>
-    </mat-toolbar>
+    
   `;
 
   CSSCodeToCopy1 = `
-    .example-spacer {
-      flex: 1 1 auto;
-    }
+    
   `;
 
   TsCodeToCopy1 = `
-    import { Component } from '@angular/core';
-    import { MatIconModule } from '@angular/material/icon';
-    import { MatButtonModule } from '@angular/material/button';
-    import { MatToolbarModule } from '@angular/material/toolbar';
-
-    @Component({
-      selector: 'app-toolbar',
-      standalone: true,
-      imports: [MatToolbarModule, MatButtonModule, MatIconModule],
-      templateUrl: './toolbar.component.html',
-      styleUrl: './toolbar.component.scss'
-    })
-    export class ToolbarComponent {}
+    
   `;
 
-  constructor() { }
+  HtmlCodeToCopy2 = `
+    
+  `;
 
+  CSSCodeToCopy2 = `
+    
+  `;
+
+  TsCodeToCopy2 = `
+    
+  `;
+
+  HtmlCodeToCopy3 = `
+    
+  `;
+
+  CSSCodeToCopy3 = `
+    
+  `;
+
+  TsCodeToCopy3 = `
+    
+  `;
+
+  HtmlCodeToCopy4 = `
+    
+  `;
+
+  CSSCodeToCopy4 = `
+    
+  `;
+
+  TsCodeToCopy4 = `
+    
+  `;
+
+  HtmlCodeToCopy5 = `
+    
+  `;
+
+  CSSCodeToCopy5 = `
+    
+  `;
+
+  TsCodeToCopy5 = `
+    
+  `;
+
+  isSmallScreen: boolean = false;
+
+  constructor(private breakpointObserver: BreakpointObserver) {
+    this.breakpointObserver
+      .observe([Breakpoints.Handset])
+      .subscribe((result) => {
+        // console.log('Is small screen:', result.matches);
+        this.isSmallScreen = result.matches;
+      });
+  }
 }
